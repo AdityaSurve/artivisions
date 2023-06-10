0f13711dbd5659aa9a18261a3e31e94d71d9ac9e
AdityaSurve <surveaditya521@gmail.com>
2023-06-10 12:15:40 +0530

    backend init

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
+
+const startServer = async () => {
+  try {
+    app.listen(8080, () => {
+      console.log(`Server is running on port 8080`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
